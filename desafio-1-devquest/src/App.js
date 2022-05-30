1cd827072dcef580c1454d1c1286154fa30c9d87
Kellyson <Kellysondias@outlook.com>
2022-05-30 17:02:01 -0300

    first commit

---
+++ desafio-1-devquest/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TextBox from './components/text/text'
+
+function App() {
+  return <TextBox 
+            style={{
+              color: color,
+              backgroundColor: backgroundColor
+            }} />;
+}
+
+export default App;
---
+++ desafio-1-devquest/src/components/text/text.js
@@ -0,0 +1,12 @@
+import './text.css'
+
+const TextBox = () => {
+    return 
+}
+
+TextBox.defaultProps = {
+    color: 'green',
+    backgroundColor: 'black'
+}
+
+export default TextBox
